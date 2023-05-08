dd6b543eb354215efe079ddde6c2c8e9d6bf4157
Omkar Pawar <pawaromkar1430@gmail.com>
2023-05-08 16:30:27 +0530

    commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+
+setup(name="Project Title",
+      version="0.0.1",
+      author="Omkar_Pawar",
+      author_email="pawaromkar1430@gmail.com",
+      packages=find_packages(),
+      install_requires=["pandas","numpy","flask"]
+      )
